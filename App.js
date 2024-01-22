07513d120e43656b56a901bdee11e9b56328c9e0
daohuybac <144346983+daohuybac@users.noreply.github.com>
2024-01-22 22:19:43 +0700

    nộp bài 1901
    nguyen thi thuy hang

--- App.js
+++ App.js
@@ -1,74 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Header from './Component/Header';
-import Footer from './Component/Footer';
-import Login from './Component/Login';
-import ListCategory from './Component/ListCategory';
-import ListProduct from './Component/ListProduct';
-import { ScrollView } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import SingleProductScreen from './Component/SingleProductScreen';
+import React from 'react';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from './Component/HomeScreen';
-import CartScreen from './Component/CartScreen';
-import { CartProvider } from './Component/CartContent';
-
+import DetailsScreen from './Component/DetailsScreen';
+import {StatusBar} from 'react-native';
+import COLORS from './consts/colors';
+import Login from './login/Login';
+import Register from './login/Register';
+import Addtocart from './Component/Addtocart';
+//import ListProduct from './Component/ListProduct'
 const Stack = createStackNavigator();
 
-export default function App() {
+const App = () => {
   return (
-    <CartProvider>
-      <View style={styles.container}>
-        {/* <Text>Hello</Text> */}
-        <StatusBar style="auto" />
-        <Header>
-
-        </Header>
-        <NavigationContainer>
-          {/* <ListCategory></ListCategory>
-        <ListProduct></ListProduct> */}
-
-          <Stack.Navigator initialRouteName="HomeScreen">
-            <Stack.Screen
-              name="HomeScreen"
-              component={HomeScreen}
-              options={{
-                headerTitle: 'Trang chủ',
-                headerTitleStyle: {
-                  fontSize: 20,
-                  fontWeight: 'bold',
-                  color: '#808080', // Mã màu hex cho xám
-                  textShadowColor: 'black', // Mã màu CSS cho đen
-                  textShadowOffset: { width: -2, height: 2 }, // Độ dịch chuyển của đổ bóng
-                  textShadowRadius: 1, // Bán kính của đổ bóng
-                  marginLeft: 130,
-                  marginTop: -20,
-                  //marginBottom:
-                },
-              }}
-            />
-            <Stack.Screen name="SingleProduct" component={SingleProductScreen} />
-          </Stack.Navigator>
-
-
-        </NavigationContainer>
-
-
-
-        {/* <Login></Login> */}
-        {/* <Footer></Footer> */}
-      </View>
-    </CartProvider>
-
+    <NavigationContainer>
+      <StatusBar backgroundColor={COLORS.white} barStyle="dark-content" />
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        {/* <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} /> */}
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="Addtocart" component={Addtocart} />
+        <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-});
+export default App;
